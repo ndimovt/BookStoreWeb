ad9f5e3d10c2447082f80b7af2e6c0e4ab16d8ee
ndimovt <115497410+ndimovt@users.noreply.github.com>
2024-02-04 11:48:01 +0200

    Add files via upload
    created insert.html for iserting records
    created delete.html for deleting
    created update.html for updating info
    created allBooks.html for getting all db records
    created byAuthor.html for getting books from specific author
    updated functions.js with new functions

--- functions.js
+++ functions.js
@@ -60,3 +60,85 @@ function updateRecordByName(){
     .catch(error => {console.log(error);});
 }
 
+function getInfo(){
+    window.open("allBooks.html", "width=75%", "heigth=75%");
+}
+
+function specificSearch(){
+    window.open("byAuthor.html", "width=75%", "heigth=75%");
+}
+function openDelete(){
+    window.open("delete.html", "width=75%", "heigth=75%");
+}
+function openUpdate(){
+    window.open("update.html", "width=75%", "heigth=75%");
+}
+function openAdd(){
+    window.open("insert.html", "width=75%", "heigth=75%");
+}
+
+/*function populateTable(){
+    let name = document.getElementById("specAuthor").value;
+    fetch("http://localhost:8003/printAll", {
+        method: "GET",
+        headers:{
+        "Accept":"application/json"},
+    })
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            let bookObj  = {
+                authorName: item.authorName,
+                bookName: item.bookName,
+                price: item.price
+            };
+            console.log(bookObj);
+        });
+    })
+}*/
+
+function populateTable(){
+    fetch("http://localhost:8003/printAll" ,{
+        method: "GET", 
+        header:{
+            "Accept":"application/json"}
+    }).then(response => response.json())
+    .then(data => {
+        const tableBody = document.querySelector("#dataTable tbody" );
+        tableBody.innerHTML = "";
+        data.forEach(item => {
+            let row = tableBody.insertRow();
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+
+            cell1.textContent = item.authorName;
+            cell2.textContent = item.bookName;
+            cell3.textContent = item.price;
+        });
+    }).catch(error => console.error(error));
+}
+
+function findByName(){    
+    let name = document.getElementById("specAuthor").value;
+    fetch("http://localhost:8003/find/" + encodeURIComponent(name),{
+        method: "GET",
+        headers:{
+            "Accept":"application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const table = document.querySelector("#dataTable tbody");
+        table.innerHTML = "";
+        data.forEach(item => {
+            let row = table.insertRow();
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+
+            cell1.textContent = item.bookName;
+            cell2.textContent = item.price;
+        });
+    }).catch(error => console.error(error));
+}
+
